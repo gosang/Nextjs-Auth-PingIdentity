0acc3b82dfb754f3591832de9df508cd1ebc7bee
gosang <gosang@outlook.com>
2024-11-13 17:38:29 +0000

    WIP auth

--- central-auth-app/next.config.ts
+++ central-auth-app/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NEXT_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath,
 };
 
 export default nextConfig;
--- central-auth-app/src/libs/forgeRockInit.ts
+++ central-auth-app/src/libs/forgeRockInit.ts
@@ -1,6 +1,25 @@
 import { Config } from "@forgerock/javascript-sdk";
+//import getConfig from "next/config";
 
 export default function forgeRockInit() {
-  const {} = process.env;
+  const {
+    FORGEROCK_CLIENT_ID,
+    FORGEROCK_REDIRECT_URI,
+    FORGEROCK_BASE_URL,
+    FORGEROCK_REALM,
+    FORGEROCK_LOGIN,
+  } = process.env;
   //const { basePath } = getConfig().publicRuntimeConfig;
+
+  Config.set({
+    clientId: FORGEROCK_CLIENT_ID,
+    redirectUri: FORGEROCK_REDIRECT_URI,
+    scope: "openid profile email",
+    serverConfig: {
+      baseUrl: FORGEROCK_BASE_URL as string,
+      timeout: 30000,
+    },
+    realmPath: FORGEROCK_REALM,
+    tree: FORGEROCK_LOGIN,
+  });
 }
