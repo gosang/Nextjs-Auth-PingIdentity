253713e76cdd7af805a8190da86bcb5ac0f3ffec
gosang <gosang@outlook.com>
2024-11-13 17:39:35 +0000

    WIP auth

--- central-auth-app/src/pages/_app.tsx
+++ central-auth-app/src/pages/_app.tsx
@@ -1,6 +1,10 @@
+import forgeRockInit from "@/libs/forgeRockInit";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+// if (typeof window === "undefined") {
+//   forgeRockInit();
+// }
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
